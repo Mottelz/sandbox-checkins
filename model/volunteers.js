9333c37cb773818630c7c79b07519ece9f355f35
Mottel Zirkind <shachna@gmail.com>
2019-02-07 19:43:37 -0500

    Renamed some files and added some placeholder functions to give an idea of how to do things.

--- model/volunteers.js
+++ model/volunteers.js
@@ -1,9 +1,21 @@
-const db = require('better-sqlite3')('checkins.db');
+const db = require('better-sqlite3')('sandbox.db');
 
 //TODO: Get the schedule for a given volunteer
+exports.getVolunteerSchedule = function(volId) {
 
-//TODO: Get the schedule for all the volunteers
+};
+
+//TODO: Get the schedule for all the volunteers for a given semester
+exports.getFullSchedule = function(semester) {
+
+};
 
 //TODO: Get the most recent checkin for a volunteer
+exports.getLastCheckin = function(volId) {
+
+};
 
 //TODO: Add a checkin for a volunteer
+exports.addCheckin = function (volId) {
+
+};
