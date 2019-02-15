2303f2030769069740aa979755faa253356f2567
Mottelz <mottelz@users.noreply.github.com>
2019-02-15 01:59:12 -0500

    Finishing the card form now redirects to hours form. The term now defaults to the current one irrelevant of what it is.

--- controllers/schedule_ctr.js
+++ controllers/schedule_ctr.js
@@ -1,4 +1,5 @@
 const schedules = require('../model/schedules.js')
+const moment = require('moment')
 
 // Add a new schedule
 exports.addSchedule = async function (data) {
@@ -7,13 +8,33 @@ exports.addSchedule = async function (data) {
     return result.changes == 1
 }
 
+// Calculates the current term
+exports.getTerm = function () {
+    // Get the month. Count starts at 0.
+    // e.g. 0 = Jan, 1 = Feb, 2 = Mar, etc.
+    let month = moment().month()
+    let year = moment().year()
+    let term = ''
+
+    //Set the term based on the month.
+    if(month >= 4 && month <= 7) {
+        term = 'Summer '
+    } else if(month >= 8 && month <= 11) {
+        term = 'Fall '
+    } else if(month >= 0 && month <= 3) {
+        term = 'Winter '
+    }
+
+    return term + year
+}
+
+
 // Lookup the schedule
 exports.getSchedule = async function (term) {
 
     let records = await schedules.getSchedulesByTerm(term)
     // Drawing this out so that:
-    // 1. It makes sense
-    // 2. Every field as a string so that the template renders correctly.
+    // It makes sense to whomever is reading this.
     let caldata = {
         "10": {"sun":"","mon":"", "tue":"", "wed": "", "thu":"", "fri":"", "sat":""},
         "11": {"sun":"","mon":"", "tue":"", "wed": "", "thu":"", "fri":"", "sat":""},
